4228feb9017bc7da81f93ccfb47d863d63f58bb0
Agunivers7 <110654906+Agunivers7@users.noreply.github.com>
2022-08-21 09:01:42 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -57,7 +57,7 @@ async def start(client, message):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_video(
-            Video=random.choice(PICS),
+            video=random.choice(PICS),
             caption=Script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             quote=True,
