ed63b1694b6be77b867ec12e58216b75e2d7b966
Agunivers7 <110654906+Agunivers7@users.noreply.github.com>
2022-09-04 21:34:04 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo, Please Wait..."
-git clone -b master https://github.com/Agunivers7/IMDb-Movie-Bot.git /IMDb-Movie-Bot
-cd /IMDb-Movie-Bot
+git clone -b master https://github.com/Agunivers7/IMDb-Movie-Bot1.git /IMDb-Movie-Bot1
+cd /IMDb-Movie-Bot1
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
 echo "Starting Bot, Please Wait..."
