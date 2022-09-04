624400a0584f3f31664c831168b4aad7ecbc812b
Agunivers7 <110654906+Agunivers7@users.noreply.github.com>
2022-09-04 22:22:54 +0530

    Update welcome.py

--- plugins/welcome.py
+++ plugins/welcome.py
@@ -7,12 +7,12 @@ from telegram.error import BadRequest
 from telegram.ext import MessageHandler, Filters, CommandHandler, run_async
 from telegram.utils.helpers import mention_markdown, mention_html, escape_markdown
 
-import tg_bot.modules.sql.welcome_sql as sql
-from tg_bot import dispatcher, OWNER_ID, LOGGER
-from tg_bot.modules.helper_funcs.chat_status import user_admin, can_delete
-from tg_bot.modules.helper_funcs.misc import build_keyboard, revert_buttons
-from tg_bot.modules.helper_funcs.msg_types import get_welcome_type
-from tg_bot.modules.helper_funcs.string_handling import markdown_parser, \
+import IMDb-Movie-Bot.modules.sql.welcome_sql as sql
+from IMDb-Movie-Bot import dispatcher, OWNER_ID, LOGGER
+from IMDb-Movie-Bot.modules.helper_funcs.chat_status import user_admin, can_delete
+from IMDb-Movie-Bot.modules.helper_funcs.misc import build_keyboard, revert_buttons
+from IMDb-Movie-Bot.modules.helper_funcs.msg_types import get_welcome_type
+from IMDb-Movie-Bot.modules.helper_funcs.string_handling import markdown_parser, \
     escape_invalid_curly_brackets
 from tg_bot.modules.log_channel import loggable
 
